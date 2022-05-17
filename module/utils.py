7c8d8dd2f69afee5c62371667e4cbbf90d28d74e
andysu <a07458666@gmail.com>
2022-05-17 17:43:16 +0800

    [add]fix encoder

--- module/utils.py
+++ module/utils.py
@@ -419,3 +419,7 @@ def accuracy(output, target, topk=(1,)):
             correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res  
+
+def set_parameter_requires_grad(model, flag = False):
+    for param in model.parameters():
+        param.requires_grad = flag
